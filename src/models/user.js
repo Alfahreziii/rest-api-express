07a6cd14d14595127b74753ae44888601772c1a9
Alfahreziii <sulthan.alfahrezy@gmail.com>
2025-05-01 12:42:49 +0700

    feat: create api users & posts using fake data

---
+++ src/models/user.js
@@ -0,0 +1,16 @@
+const USERS = [
+    {
+      id: 1,
+      name: "Test User",
+      email: "test@example.com",
+    },
+  ];
+  
+  const all = () => {
+    return USERS;
+  };
+  
+  module.exports = {
+    all,
+  };
+  
